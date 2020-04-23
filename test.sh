a2be288694c6394bef33281ed2abb6095c67afd0
Kohei Yamaguchi <fix7211@gmail.com>
2020-04-23 14:17:47 +0000

    Modify try -> assert

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-try() {
+assert() {
     expected="$1"
     input="$2"
 
@@ -18,25 +18,25 @@ try() {
     fi
 }
 
-try 0 0
-try 42 42
-try 21 '5+20-4'
-try 41 " 12 + 34 - 5 "
-try 47 "5+6*7"
-try 15 "5*(9-6)"
-try 4 "8/2"
-try 4 "(3+5)/2"
-try 1 "a=1;"
-try 100 "a = 73 + 27;"
-try 4 "b = 4"
-try 10 "a = b = 10;"
-try 15 "c = 3 + 7; c + 5;"
-try 10 "a = 3; b = a + 7;"
-try 10 "a = 3; b = 7; c = a + b;"
-try 1 "5 == 5;"
-try 1 "99 != 98;"
-try 0 "2 == 5;"
-try 0 "10 != 10;"
-try 1 "a = 3; a == 3;"
+assert 0 0
+assert 42 42
+assert 21 '5+20-4'
+assert 41 " 12 + 34 - 5 "
+assert 47 "5+6*7"
+assert 15 "5*(9-6)"
+assert 4 "8/2"
+assert 4 "(3+5)/2"
+assert 1 "a=1;"
+assert 100 "a = 73 + 27;"
+assert 4 "b = 4"
+assert 10 "a = b = 10;"
+assert 15 "c = 3 + 7; c + 5;"
+assert 10 "a = 3; b = a + 7;"
+assert 10 "a = 3; b = 7; c = a + b;"
+assert 1 "5 == 5;"
+assert 1 "99 != 98;"
+assert 0 "2 == 5;"
+assert 0 "10 != 10;"
+assert 1 "a = 3; a == 3;"
 
 echo Test OK
