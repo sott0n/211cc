5c28d3eb80d339fb96cbeb71f52e25ed3faa864c
Kohei Yamaguchi <fix7211@gmail.com>
2020-05-06 21:41:49 +0900

    Change size of `int` from 8 to 4

--- type.c
+++ type.c
@@ -1,7 +1,7 @@
 #include "211cc.h"
 
 Type *ty_char = &(Type){TY_CHAR, 1, 1};
-Type *ty_int = &(Type){TY_INT, 8, 8};
+Type *ty_int = &(Type){TY_INT, 4, 4};
 
 static Type *new_type(TypeKind kind, int size, int align) {
     Type *ty = malloc(sizeof(Type));
