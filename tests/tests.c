c977ef1232383057864904bf981e86ca9200db58
Kohei Yamaguchi <fix7211@gmail.com>
2020-05-08 00:02:29 +0900

    Add `long long` as an alias for `long`

--- tests/tests.c
+++ tests/tests.c
@@ -288,6 +288,8 @@ int main() {
     assert(4, ({ int x; sizeof(x); }), "({ int x; sizeof(x); })");
     assert(8, ({ long int x; sizeof(x); }), "({ long int x; sizeof(x); })");
     assert(8, ({ int long x; sizeof(x); }), "({ int long x; sizeof(x); })");
+    assert(8, ({ long long x; sizeof(x); }), "({ long long x; sizeof(x); })");
+    assert(8, ({ long long int x; sizeof(x); }), "({ long long int x; sizeof(x); })");
 
     printf("OK\n");
     return 0;
