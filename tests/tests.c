5176b96adf69fdec5cbe77cbc5b1a87dc2436ac3
Kohei Yamaguchi <fix7211@gmail.com>
2020-05-19 23:36:34 +0900

    Implement array-to-pointer decay in the func param context

--- tests/tests.c
+++ tests/tests.c
@@ -72,6 +72,8 @@ _Bool bool_fn_sub(_Bool x) { return x - 1; }
 
 static int static_fn() { return 3; }
 
+int param_decay(int x[]) { return x[0]; }
+
 int main() {
     assert(0, 0, "0");
     assert(42, 42, "42");
@@ -502,6 +504,8 @@ int main() {
 
     assert(8, sizeof(int(*)[10]), "sizeof(int(*)[10])");
     assert(8, sizeof(int(*)[][10]), "sizeof(int(*)[][10])");
+
+    assert(3, ({ int x[2]; x[0]=3; param_decay(x); }), "({ int x[2]; x[0]=3; param_decay(x); })");
 
     printf("OK\n");
     return 0;
